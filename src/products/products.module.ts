6d006263118601c082742bf6a6ceb16b440af09a
Juan Manuel Costa Pintos <costapintosjm@gmail.com>
2023-06-22 12:04:33 -0300

    OneToManyManyToOne

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,14 +1,18 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProductsService } from './products.service';
-import { ProductsController } from './products.controller';
-import { Product } from './entities/product.entity';
+import { Module }                from '@nestjs/common';
+import { TypeOrmModule }         from '@nestjs/typeorm';
+  
+import { ProductsService }       from './products.service';
+import { ProductsController }    from './products.controller';
+import { Product, ProductImage } from './entities';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
   imports: [
-    TypeOrmModule.forFeature([Product]),
+    TypeOrmModule.forFeature([
+      Product,
+      ProductImage
+    ]),
   ]
 })
 export class ProductsModule {}
